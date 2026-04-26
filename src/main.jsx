efe0679d8498aa68315000426294d72f0d947a81
Raja <vp honda>
2026-04-26 11:08:22 +0530

    Fix: PWA app install + smart feature

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 import './index.css';
 
 // ⭐ Register PWA Service Worker
